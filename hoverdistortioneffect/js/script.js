3e0a81de93a8f087d92a501edc89d2d81bcd77c7
NAYMUR143 <66328660+NAYMUR143@users.noreply.github.com>
2022-01-15 18:54:26 +0600

    Add files via upload

---
+++ hoverdistortioneffect/js/script.js
@@ -0,0 +1,75 @@
+const Locoscroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  smartphone: {
+    smooth: true,
+  },
+  //   reloadOnContextChange: true,
+});
+
+//hover effect
+
+var hoverDistort1 = new hoverEffect({
+  parent: document.querySelector("#pic01"),
+  intensity: 0.5,
+  image1: "img/1.jpg",
+  image2: "img/2.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort2 = new hoverEffect({
+  parent: document.querySelector("#pic02"),
+  intensity: 0.5,
+  image1: "img/4.jpg",
+  image2: "img/3.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort3 = new hoverEffect({
+  parent: document.querySelector("#pic03"),
+  intensity: 0.5,
+  image1: "img/6.jpg",
+  image2: "img/7.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort4 = new hoverEffect({
+  parent: document.querySelector("#pic04"),
+  intensity: 0.5,
+  image1: "img/9.jpg",
+  image2: "img/10.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort5 = new hoverEffect({
+  parent: document.querySelector("#pic05"),
+  intensity: 0.5,
+  image1: "img/5.jpg",
+  image2: "img/3.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort6 = new hoverEffect({
+  parent: document.querySelector("#pic06"),
+  intensity: 0.5,
+  image1: "img/8.jpg",
+  image2: "img/01.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort6 = new hoverEffect({
+  parent: document.querySelector("#pic07"),
+  intensity: 0.5,
+  image1: "img/3.jpg",
+  image2: "img/02.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
+var hoverDistort6 = new hoverEffect({
+  parent: document.querySelector("#pic08"),
+  intensity: 0.5,
+  image1: "img/4.jpg",
+  image2: "img/1.jpg",
+  displacementImage: "img/displacement.png",
+  imagesRatio: 1.7,
+});
